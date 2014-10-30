932c7e5af0eebc96f7a629ed8d401c6e4d4b1e48
Chris Smowton <christopher.smowton@cruk.manchester.ac.uk>
2014-10-30 21:21:23 +0200

    Shell script: don't depend on working directory

--- ccgrid_eval_scripts/benchmark_start_job.sh
+++ ccgrid_eval_scripts/benchmark_start_job.sh
@@ -2,7 +2,9 @@
 
 # Arg 1 is the job directory
 
-runner_for_job=$(python ./get_cluster_hostname.py ~/noflex_cluster.json $1)
+base=$(dirname $0)
+
+runner_for_job=$(python $base/get_cluster_hostname.py ~/noflex_cluster.json $1)
 
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -R 8080:localhost:8080 $runner_for_job java -Djava.io.tmpdir=$1 -cp /mnt/nfs/gatk/Queue-3.1-smowton.jar:/mnt/nfs/gatk/json-org.jar:/mnt/nfs/gatk/queue_jobrunner org.broadinstitute.sting.queue.QCommandLine -S /mnt/nfs/gatk/gatk_pipeline_nowrite.scala -run -jobSGDir $1 --scattercount 1 --input /mnt/nfs/inputs/226_normal.gatk.bam --workdir /mnt/nfs/inputs/inter_normal/ --fakedir $1 --singlequeue -jobRunner Scan
 
