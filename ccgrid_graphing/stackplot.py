997adbf7fc8bfa54d9b3eedef27b95bd25620903
Christopher Smowton <christopher.smowton@cruk.manchester.ac.uk>
2014-11-02 15:15:49 +0000

    Stackplot script: add titles, fix layout for single-column sizing.
    Summary script: also generate pipeline execution time histogram

--- ccgrid_graphing/stackplot.py
+++ ccgrid_graphing/stackplot.py
@@ -12,18 +12,23 @@ series_markers = [' ', '|']
 
 def draw_stackplot(plots, xlabel, ylabel, save_file = None):
 
-    fig = plt.figure()
+    fig = plt.figure(figsize = (4, len(plots)))
+
     masterax = fig.add_subplot(111, axisbg='none')
     masterax.spines['top'].set_color('none')
     masterax.spines['bottom'].set_color('none')
     masterax.spines['left'].set_color('none')
     masterax.spines['right'].set_color('none')
     masterax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+    masterax.set_xticks([])
+    masterax.set_yticks([])
 
-    for i, plot in enumerate(plots):
+    for i, (title, plot) in enumerate(plots):
 
         arg = int("%s%s%s" % (len(plots), 1, i+1))
         ax = fig.add_subplot(arg)
+
+        ax.locator_params(axis='y', nbins=4)
 
         for ((xs, ys), colour, marker) in zip(plot, series_colours, series_markers):
             ax.plot(xs, ys, color=colour, marker=marker)
@@ -35,7 +40,11 @@ def draw_stackplot(plots, xlabel, ylabel, save_file = None):
         lims = ax.get_ylim()
         ax.set_ylim(lims[0], lims[1] * 1.1)
 
-    masterax.set_ylabel(ylabel)
+        ax.set_title(title)
+    
+    masterax.set_ylabel(ylabel, labelpad = 25)
+
+    fig.tight_layout()
 
     if save_file is None:
         plt.show()
