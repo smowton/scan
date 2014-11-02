b87f59f54e73747425066775e3644689c34918e7
Christopher Smowton <christopher.smowton@cruk.manchester.ac.uk>
2014-11-02 17:45:39 +0000

    Stackplot: tolerate one-plot case

--- ccgrid_graphing/stackplot.py
+++ ccgrid_graphing/stackplot.py
@@ -14,19 +14,24 @@ def draw_stackplot(plots, xlabel, ylabel, save_file = None):
 
     fig = plt.figure(figsize = (4, len(plots)))
 
-    masterax = fig.add_subplot(111, axisbg='none')
-    masterax.spines['top'].set_color('none')
-    masterax.spines['bottom'].set_color('none')
-    masterax.spines['left'].set_color('none')
-    masterax.spines['right'].set_color('none')
-    masterax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
-    masterax.set_xticks([])
-    masterax.set_yticks([])
+    if len(plots) > 1:
+
+        masterax = fig.add_subplot(111, axisbg='none')
+        masterax.spines['top'].set_color('none')
+        masterax.spines['bottom'].set_color('none')
+        masterax.spines['left'].set_color('none')
+        masterax.spines['right'].set_color('none')
+        masterax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+        masterax.set_xticks([])
+        masterax.set_yticks([])
 
     for i, (title, plot) in enumerate(plots):
 
         arg = int("%s%s%s" % (len(plots), 1, i+1))
         ax = fig.add_subplot(arg)
+
+        if len(plots) == 1:
+            masterax = ax
 
         ax.locator_params(axis='y', nbins=4)
 
@@ -38,7 +43,12 @@ def draw_stackplot(plots, xlabel, ylabel, save_file = None):
         else:
             ax.set_xlabel(xlabel)
         lims = ax.get_ylim()
-        ax.set_ylim(lims[0], lims[1] * 1.1)
+        span = lims[1] - lims[0]
+        addspan = span * 0.1
+        newbottom = lims[0] - addspan if lims[0] != 0 else lims[0]
+        newtop = lims[1] + addspan
+
+        ax.set_ylim(newbottom, newtop)
 
         ax.set_title(title)
     
