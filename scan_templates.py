6197ae91f17b14188f6f5183527a6345bdc1e7d1
Christopher Smowton <christopher.smowton@cruk.manchester.ac.uk>
2014-11-18 13:41:15 +0000

    Templates: run start_gatk_pipeline.sh from correct location.

--- scan_templates.py
+++ scan_templates.py
@@ -5,6 +5,6 @@ def templates():
                             "script": 'echo "%s" > /tmp/hello_world',
                             "classname": "gatk_rtc"},
             "gatk": {"desc": "Genome Analysis (GATK)", 
-                     "script": "/root/scan/dummy_clients/start_gatk_pipeline.sh %s 1 /mnt/nfs /mnt/nfs/test_workdir /mnt/nfs/Queue-3.1-smowton.jar",
+                     "script": "cd ~/scan; ./dummy_clients/start_gatk_pipeline.sh %s 1 /mnt/nfs /mnt/nfs/test_workdir /mnt/nfs/Queue-3.1-smowton.jar",
                      "classname": "gatk_queue_runner"}}
 
