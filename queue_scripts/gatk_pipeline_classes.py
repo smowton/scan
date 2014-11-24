baa78ff082667ce381b81dde186ed32a3f344b75
Christopher Smowton <christopher.smowton@cruk.manchester.ac.uk>
2014-11-24 17:14:17 +0000

    Pipeline classes: permit specifiying initial machine type

--- queue_scripts/gatk_pipeline_classes.py
+++ queue_scripts/gatk_pipeline_classes.py
@@ -1,9 +1,9 @@
 
-def getclasses(gatk_ssh_username = "user"):
+def getclasses(gatk_ssh_username = "user", gatk_init_cores = "1", gatk_init_mem = "4096"):
 
     return [{"name": "gatk_%s" % x, 
              "user": gatk_ssh_username, 
              "respath": "/home/user/csmowton/scan/getres.py",
-             "init_hwspec": {"cores": 1, "memory": 4096}}
+             "init_hwspec": {"cores": int(gatk_init_cores), "memory": int(gatk_init_mem)}}
             for x in ["rtc", "ir", "ir_gather", "br", "pr", "pr_gather", "ug", "ug_gather", "vf", "vf_gather", "ve", "queue_runner"]]
 
