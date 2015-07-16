9b036be3f73e9a8560e8342b719d61193be15bae
Chris <chris@smowton.net>
2015-07-16 14:14:21 +0300

    Update del_this_worker

--- scripts/del_this_worker.sh
+++ scripts/del_this_worker.sh
@@ -2,7 +2,7 @@
 
 # Releases this worker. Blocks until it is ready to stop (perhaps a long time) and prints its details in the format classname:hostname:id
 
-~/scan/blocking_delete.py `cat ~/scan_worker_class` `cat ~/scan_sched_address` `cat ~/scan_worker_id`
+~/scan/blocking_delete.py `cat ~/scan_sched_address` `cat ~/scan_worker_id`
 
 
 
