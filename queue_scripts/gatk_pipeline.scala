5de4daccd9de13ff42c95bd90b353ccfddc9cd06
Chris Smowton <chris@smowton.net>
2015-09-07 20:28:33 +0300

    Make some arguments nonessential

--- queue_scripts/gatk_pipeline.scala
+++ queue_scripts/gatk_pipeline.scala
@@ -132,10 +132,10 @@ class VarCallingPipeline extends QScript {
   @Argument
   var refdir : String = _
 
-  @Argument
+  @Argument(required=false)
   var scattercount : String = ""
 
-  @Argument
+  @Argument(required=false)
   var singleQueue : Boolean = false
 
   def parseScatterCount : Seq[Int] = {
