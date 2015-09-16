b9f02fb68cf7e77630c7c57ddac87d6f4efac73e
Chris Smowton <christopher.smowton@cruk.manchester.ac.uk>
2015-09-16 10:08:27 +0100

    Typo

--- scripts/handle_prescale.sh
+++ scripts/handle_prescale.sh
@@ -4,5 +4,5 @@ case $SLIPSTREAM_SCALING_ACTION in
   vm_remove)
       ~/scan/scripts/del_this_worker.sh ;;
   disk_detach)
-      python ~/scan/btrfs_scripts/remove-device.py `ss-set disk.detach.device` ;;
+      python ~/scan/btrfs_scripts/remove-device.py `ss-get disk.detach.device` ;;
 esac
